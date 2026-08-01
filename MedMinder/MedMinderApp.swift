68c307aa1ba0387265810955ab03809cdd5b294c
David Carranco <david.carranco@icloud.com>
2026-08-01 11:04:44 -0400

    Initial Commit

---
+++ MedMinder/MedMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedMinderApp.swift
+//  MedMinder
+//
+//  Created by David Carranco on 2026-08-01.
+//
+
+import SwiftUI
+
+@main
+struct MedMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
